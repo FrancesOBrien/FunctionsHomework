e0c2689d1a92f8cc9de65819452d2f22597df4bd
FrancesOBrien <114510060+FrancesOBrien@users.noreply.github.com>
2022-10-12 16:45:18 -0400

    just functionsDrills, please

---
+++ day1.js
@@ -0,0 +1,96 @@
+// //Prompt 1: I want you to write a function that has a dog age, the function is for the dogs birthday and will increase the dogs age by 1.
+// Prompt 2: I want you to make an array of dog names with 5 dogs. The dogs are at doggy daycare. The last dog in the array got picked up and three new dogs arrived
+// Prompt 3: I want you to make a user object with 5 properties (name, age, email, friends, and hobbies). ONE function will add 1 to the age, ANOTHER FUNCTION will add a new friend.
+
+// let dog = {
+//     name: "Teacake",
+//     age: 4,
+//     birthdate: "06-12-2018"
+
+// let dog = {
+//     name : "fido",
+//     birthdate : new Date(2022, 9, 11),
+//     color: 'brown',
+//     breed: "German Sheppard",
+//     age : 2
+//     }
+// const isItMyDogsBirthday = (dog) => {
+//     let currentDate = new Date();
+//     birthdate = dog.birthdate;
+//     if(currentDate.getMonth() === birthdate.getMonth() && currentDate.getDay() === birthdate.getDay()) {
+//          dog.age++;
+//     }
+// }
+// console.log(dog.age);
+// isItMyDogsBirthday(dog);
+// console.log(dog.age);
+// }
+// function trackDogAge(){
+//     dog.age = dog.age + 1
+//     console.log(dog.age)
+//     console.log(dog)
+// }
+// trackDogAge();
+
+// let dayCareRoster = ["Oscar", "Teacake", "Lola", "Heywood", "Mitzy"];
+//     dayCareRoster.pop()
+//     dayCareRoster.push("Jaeger", "Brulee", "Killian")
+//     console.log(dayCareRoster)
+
+
+// let user = {
+//     name: "Teacake",
+//     age: 4,
+//     email: "teacake@gmail.com",
+//     friends: ["Oscar", "Mitzy", "Lana"],
+//     hobbies: ["Kick Fetch", "Cuddling", "Basking"],
+// }
+
+// function addAYear(){
+//     user.age = user.age + 1
+//     console.log(user)
+// }
+// addAYear() //call this every birthday?
+
+// function addABuddy(){
+//     user.friends.push("Jaeger")
+//     console.log(user)
+// }
+// addABuddy()
+
+// let studentGrade = 80
+// const grade = (parameter) = {
+//     studentGrade = parameter
+// }
+// grade(80);
+// console.log('Grade ', studentGrade)
+// grade(55);
+// grade(99);
+
+// let kindergartenClass = ["Maddalena", "Han", "Arissa", "Darren", "Malu"];
+
+// const addStudent(name) => {
+//     kindergartenClass.push(name)
+// }
+//     addStudent: ("Mika");
+//     addStudent: ("Jorge");
+//     addStudent: ("Locke")
+// console.log(kindergartenClass)
+// return addStudent()
+
+// let presentDogs = ["Oscar", "Teacake", "Lola", "Heywood", "Mitzy"];
+
+// function addDogs(newDogs) {
+//     let newDogs = ["Jaeger", "Brulee", "Hero"]
+//     presentDogs.push(newDogs)
+//     console.log(presentDogs)
+// }
+// return addDogs
+
+
+
+
+
+
+
+
